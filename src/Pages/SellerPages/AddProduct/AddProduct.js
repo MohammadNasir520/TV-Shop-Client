ed81fa452ef910aa9ee323a25ea18610868f6202
MohammadNasir520 <nasirahsan520@gmail.com>
2022-11-29 19:30:42 +0600

    redirect problem solved

--- src/Pages/SellerPages/AddProduct/AddProduct.js
+++ src/Pages/SellerPages/AddProduct/AddProduct.js
@@ -47,8 +47,8 @@ const AddProduct = () => {
             .then(res => res.json())
             .then(data => {
                 console.log(data)
-                toast.success('product added successfully')
-                navigate('/sellerDashboard/myProducts')
+                toast.success('TV added successfully')
+                navigate('/dashboard/myproducts')
             })
     }
     return (
